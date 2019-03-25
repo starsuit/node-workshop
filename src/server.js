744299ba64047ef4a06e6c2323a0053e7d9e2aa6
Joko Sanyang <jo.san@live.co.uk>
2019-03-25 15:14:55 +0000

    add .gitignore

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ const http = require("http");
 const fs = require('fs');
 
 const handler = (request, response) => {
+    console.log(request);
     const endpoint = request.url;
     const method = request.method;
-    console.log({endpoint, method});
-    if (endpoint === '/'){
+    console.log({endpoint});
+    if (endpoint === '/') {
         response.writeHead(200, {"Content-Type": "text/html"});
     fs.readFile(__dirname + '/..' + '/public/index.html', (error,file) => {
         if (error) {
@@ -14,17 +15,37 @@ const handler = (request, response) => {
         }
         response.end(file);
     })
-}
-   
-
+} else if (endpoint.indexOf("public") != -1) {
+    const extension = endpoint.split(".")[1];
+    console.log(extension);
+    const extensionType = {
+        html: "text/html",
+        css: "text/css",
+        js: "application/javascript",
+        jpg: "image/jpeg"
+    }
+    fs.readFile(__dirname + '/..' + endpoint, (error,file) => {
+        if (error) {
+            console.log(error);
+            return;
+        } else {
+            response.writeHead(200, {"Content-Type": extensionType[extension]});
+            response.end(file);
+        }
+        
+});
+} else {
+    response.writeHead(404);
+    response.end("404 not found");
+};
 }
 
 const server = http.createServer(handler);
 
 const message = "I am so happy to be part of FAC16!"
 
-server.listen(3000, () => {
-    console.log("Server is listening on port 3000. Ready to accept requests");
+server.listen(3060, () => {
+    console.log("Server is listening on port 3050. Ready to accept requests");
 }
 );
 
