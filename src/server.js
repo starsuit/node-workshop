c29e263305b51167252bd8c333cba441a6b533bf
Joko Sanyang <jo.san@live.co.uk>
2019-03-25 14:01:19 +0000

    Send first server response

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 const http = require("http");
-const server = http.createServer();
+
+const handler = (request, response) => {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(message);
+    response.end();
+}
+
+const server = http.createServer(handler);
+
+const message = "I am so happy to be part of FAC16!"
 
 server.listen(3000, () => {
     console.log("Server is listening on port 3000. Ready to accept requests");
 }
 );
+
