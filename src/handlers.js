d10d25367a83ee0b9c1761a034a5b344b33529cc
Samantha Dickeson <samlucydickeson@gmail.com>
2019-03-26 11:49:47 +0000

    reorganised files

---
+++ src/handlers.js
@@ -0,0 +1,60 @@
+const http = require("http");
+const fs = require("fs");
+const querystring = require("querystring");
+const message = "I am so happy to be part of FAC16!";
+
+const handler = (request, response) => {
+  const endpoint = request.url;
+  if (request.method === "POST") {
+    let allTheData = "";
+    request.on("data", chunkOfData => {
+      allTheData += chunkOfData;
+    });
+    request.on("end", () => {
+      const convertedData = querystring.parse(allTheData);
+      console.log(convertedData);
+      response.end();
+    });
+  }
+  if (endpoint === "/") {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    fs.readFile(__dirname + "/.." + "/public/index.html", (error, file) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint === "/create-post") {
+    response.writeHead(301, { Location: "/" });
+    fs.readFile(__dirname + "/.." + "/public/index.html", (error, file) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      response.end(file);
+    });
+  } else if (endpoint.indexOf("public") != -1) {
+    const extension = endpoint.split(".")[1];
+    const extensionType = {
+      html: "text/html",
+      css: "text/css",
+      js: "application/javascript",
+      jpg: "image/jpeg"
+    };
+    fs.readFile(__dirname + "/.." + endpoint, (error, file) => {
+      if (error) {
+        console.log(error);
+        return;
+      } else {
+        response.writeHead(200, { "Content-Type": extensionType[extension] });
+        response.end(file);
+      }
+    });
+  } else {
+    response.writeHead(404);
+    response.end("404 not found");
+  }
+};
+
+module.exports = handler;
--- src/server.js
+++ src/server.js
@@ -1,73 +1,7 @@
 const http = require("http");
-const fs = require('fs');
-const querystring = require('querystring');
-
-const handler = (request, response) => {
-    const endpoint = request.url;
-    const method = request.method;
-    if (request.method === "POST") {
-        let allTheData = '';
-        request.on('data', (chunkOfData) => {
-        allTheData += chunkOfData;
-})
-request.on('end', () => {
-    const convertedData = querystring.parse(allTheData);
-    console.log(convertedData);
-    response.end();
-})}
-    if (endpoint === '/') {
-        response.writeHead(200, {"Content-Type": "text/html"});
-    fs.readFile(__dirname + '/..' + '/public/index.html', (error,file) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
-        response.end(file);
-    })
-}
-else if (endpoint === '/create-post') {
-    response.writeHead(301, {"Location": "/"});
-fs.readFile(__dirname + '/..' + '/public/index.html', (error,file) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-    response.end(file);
-})
-}
-else if (endpoint.indexOf("public") != -1) {
-    const extension = endpoint.split(".")[1];
-    const extensionType = {
-        html: "text/html",
-        css: "text/css",
-        js: "application/javascript",
-        jpg: "image/jpeg"
-    }
-    fs.readFile(__dirname + '/..' + endpoint, (error,file) => {
-        if (error) {
-            console.log(error);
-            return;
-        } else {
-            response.writeHead(200, {"Content-Type": extensionType[extension]});
-            response.end(file);
-        }
-        
-});
-} else {
-    response.writeHead(404);
-    response.end("404 not found");
-};
-
-}
-
-
+const handler = require("./handlers");
 
 const server = http.createServer(handler);
-
-const message = "I am so happy to be part of FAC16!"
-
 server.listen(3060, () => {
-    console.log("Server is listening on port 3050. Ready to accept requests");
-}
-);
-
+  console.log("Server is listening on port 3060. Ready to accept requests");
+});
